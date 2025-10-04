1cc5ddddcdcead45fbc4ffa8bc551f8537312ae6
zunaid.hassan <67002737+zunaid-hassan@users.noreply.github.com>
2025-10-05 02:05:54 +0800

    Solutions to Functions

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,11 +1,17 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (a) {
+  let celsius = (a - 32) * (5 / 9);
+  let celsiusRounded = Math.round(celsius * 10) / 10;
+  return celsiusRounded;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (b) {
+  let fahrenheit = b * (9 / 5) + 32;
+  let fahrenheitRounded = Math.round(fahrenheit * 10) / 10;
+  return fahrenheitRounded;
 };
 
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
-  convertToFahrenheit
+  convertToFahrenheit,
 };
