244acbac99ce9b6acb989d337326b671874ca66d
zunaid.hassan <67002737+zunaid-hassan@users.noreply.github.com>
2026-07-26 17:55:47 +0800

    Complete 13_Palindromes exercise

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,5 +1,32 @@
-const palindromes = function () {
+const palindromes = function (a) {
+  let a2 = a
+    .split(", ")
+    .join()
+    .split(" ")
+    .join()
+    .toLowerCase()
+    .split(",")
+    .join();
+  let a3 = "";
+  for (let i = 0; i < a2.length; i++) {
+    if (![".", "!", ","].includes(a2[i])) {
+      a3 += a2[i];
+    }
+  }
+  let a3Arr = a3.split("");
 
+  let a3ArrRev = a3Arr.toReversed();
+  let bool = true;
+
+  for (let i = 0; i < a3Arr.length; i++) {
+    if (a3Arr[i] === a3ArrRev[i]) {
+      bool = true;
+    } else {
+      bool = false;
+      return bool;
+    }
+  }
+  return bool;
 };
 
 // Do not edit below this line
--- foundations/13_palindromes/palindromes.spec.js
+++ foundations/13_palindromes/palindromes.spec.js
@@ -1,25 +1,25 @@
-const palindromes = require('./palindromes')
+const palindromes = require("./palindromes");
 
-describe('palindromes', () => {
-  test('detects palindrome', () => {
-    expect(palindromes('racecar')).toBe(true);
+describe("palindromes", () => {
+  test("detects palindrome", () => {
+    expect(palindromes("racecar")).toBe(true);
   });
-  test.skip('detects palindrome with numbers', () => {
-    expect(palindromes('rac3e3car')).toBe(true);
+  test("detects palindrome with numbers", () => {
+    expect(palindromes("rac3e3car")).toBe(true);
   });
-  test.skip('detects palindrome with multiple words', () => {
-    expect(palindromes('A car, a man, a maraca.')).toBe(true);
+  test("detects palindrome with multiple words", () => {
+    expect(palindromes("A car, a man, a maraca.")).toBe(true);
   });
-  test.skip('ignores punctuation', () => {
-    expect(palindromes('racecar!')).toBe(true);
+  test("ignores punctuation", () => {
+    expect(palindromes("racecar!")).toBe(true);
   });
-  test.skip('is case insensitive', () => {
-    expect(palindromes('Racecar!')).toBe(true);
+  test("is case insensitive", () => {
+    expect(palindromes("Racecar!")).toBe(true);
   });
-  test.skip('detects non-palindromes', () => {
-    expect(palindromes('ZZZZ car, a man, a maracaz.')).toBe(false);
+  test("detects non-palindromes", () => {
+    expect(palindromes("ZZZZ car, a man, a maracaz.")).toBe(false);
   });
-  test.skip('detects non-palindromes with numbers', () => {
-    expect(palindromes('r3ace3car')).toBe(false);
+  test("detects non-palindromes with numbers", () => {
+    expect(palindromes("r3ace3car")).toBe(false);
   });
 });
