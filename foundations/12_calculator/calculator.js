76b1d00e803e88675a45adf72ff38a74cc5d61cd
zunaid.hassan <67002737+zunaid-hassan@users.noreply.github.com>
2026-07-26 17:04:27 +0800

    Finish 12_Calculator exercise

--- foundations/12_calculator/calculator.js
+++ foundations/12_calculator/calculator.js
@@ -1,25 +1,34 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a+b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a-b;
 };
 
-const sum = function() {
-	
+const sum = function(a) {
+	return a.reduce((acc, current) => acc+current, 0)
 };
 
-const multiply = function() {
-
+const multiply = function(a) {
+  return a.reduce((acc, current) => acc*current, 1)
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return a**b;
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+    let product = 1;
+    if (a === 0) {
+        return 1
+    } else {
+        for (let i = a; i > 0; i--) {
+            product *= i;
+
+        }
+        return product;
+    }
 };
 
 // Do not edit below this line
