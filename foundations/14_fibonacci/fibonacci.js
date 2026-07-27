630c6240e4ea3cdfeeb200319a365b281bb9c80d
zunaid.hassan <67002737+zunaid-hassan@users.noreply.github.com>
2026-07-27 20:52:31 +0800

    work in progress for 14_fibonacci exercise

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,5 +1,11 @@
-const fibonacci = function() {
-
+const fibonacci = function (a) {
+  if (a <= 0) {
+    return "OOPS";
+  } else if (a === 1 || a === 2) {
+    return 1;
+  } else {
+    let index = a - 1;
+  }
 };
 
 // Do not edit below this line
