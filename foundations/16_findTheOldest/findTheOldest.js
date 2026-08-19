a69b7ad53104825606b934b0fd017573d411c9d9
zunaid.hassan <67002737+zunaid-hassan@users.noreply.github.com>
2026-08-19 15:16:31 +0800

    Finish JS Foundaitons Exercise!

--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,13 +1,25 @@
-const findTheOldest = function (peopleList) {
+function getAge(people) {
   const dateCurrent = new Date();
   const yearCurrent = dateCurrent.getFullYear();
+  let age = 0;
 
-  let ageList = [];
-
-  for (i = 0; i < peopleList.length; i++) {
-    ageList.push(peopleList[i].yearOfDeath - peopleList[i].yearOfBirth);
+  if (people.yearOfDeath != undefined) {
+    age = people.yearOfDeath - people.yearOfBirth;
+    return age;
+  } else {
+    age = yearCurrent - people.yearOfBirth;
+    return age;
   }
-  return ageList.sort;
+}
+
+const findTheOldest = function (peopleList) {
+  return peopleList.reduce((oldest, current) => {
+    if (getAge(oldest) > getAge(current)) {
+      return oldest;
+    } else {
+      return current;
+    }
+  });
 };
 
 // Do not edit below this line
